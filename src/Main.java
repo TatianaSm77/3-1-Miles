3c1e53bfdcdf1d4527df0fe4aaf6d1126a06654e
Tatiana Smirnova <redkiivid@mail.ru>
2023-03-10 23:36:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 7500;
+        int oneMile = 20;
+        int bonusMiles = ticketPrice / oneMile;
+
+
+        System.out.println("Вам начисленно " + bonusMiles + " бонусных миль");
+    }
+}
