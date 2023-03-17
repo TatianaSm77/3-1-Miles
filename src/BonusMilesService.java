c867e583a0a9ea1fb4edf5b6011d4fa2bbfde03d
Tatiana Smirnova <redkiivid@mail.ru>
2023-03-17 18:32:15 +0300

    new commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate (int price) {
+        int oneMiles = 20;
+
+        int miles = price / oneMiles;
+        return miles;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 7500;
-        int oneMile = 20;
-        int bonusMiles = ticketPrice / oneMile;
-
-
-        System.out.println("Вам начисленно " + bonusMiles + " бонусных миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 1000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
